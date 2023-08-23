09de24aefa7429268f3b792db49bd96aee945268
Parkseungje <tmdwprpdla@naver.com>
2023-08-24 02:41:14 +0900

    아이디 중복검사 추가 및 아이디값 미입력시 에러처리

--- java/com/react/exam/api/user/service/UserService.java
+++ java/com/react/exam/api/user/service/UserService.java
@@ -1,20 +1,31 @@
 package com.react.exam.api.user.service;
 
+import com.react.exam.api.user.dto.UserRequest;
 import com.react.exam.api.user.entity.User;
 import com.react.exam.api.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-@RequiredArgsConstructor
 @Transactional
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public void saveUser(User user) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void saveUser(UserRequest userRequest, PasswordEncoder passwordEncoder){
+        User.UserBuilder builder = User.builder();
+        builder.userId(userRequest.getUserId());
+        builder.password(passwordEncoder.encode(userRequest.getPassword()));
+
+        User user = builder.build();
+
         validateDuplicateMember(user);
         userRepository.save(user);
     }
@@ -27,11 +38,36 @@ public class UserService {
         return userObj;
     }
 
-    private void validateDuplicateMember(User user) {
-        User findMember = userRepository.findByUserId(user.getUserId());
-        if (findMember != null) {
-            throw new IllegalStateException("이미 가입된 회원입니다.");
+    //회원가입시 한번더 중복검사
+    private void validateDuplicateMember(User user){
+        try{
+            duplicateFunc(user);
+        }
+        catch (Exception e){
+            throw new IllegalStateException(e.getMessage());
         }
     }
 
+    //프론트에서 id중복검사 api
+    public void checkDuplicateId(UserRequest userRequest){
+        User.UserBuilder builder = User.builder();
+        builder.userId(userRequest.getUserId());
+
+        User user = builder.build();
+
+        try{
+            duplicateFunc(user);
+        }
+        catch (Exception e){
+            throw new IllegalStateException(e.getMessage());
+        }
+
+    }
+
+    private void duplicateFunc(User user){
+        User findMember = userRepository.findByUserId(user.getUserId());
+
+        if(user.getUserId() == "") throw new IllegalStateException("값을 입력해주세요.");
+        if(findMember != null) throw new IllegalStateException("이미 가입된 회원입니다.");
+    }
 }
